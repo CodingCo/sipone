67215a70f4cf31ba52a0694d6ba49f27226ab26b
ThomasHedegaard <thomashj93@gmail.com>
2015-03-01 21:07:04 +0100

    JPA ready, but problems with Ojdbc
    IMPORTANT: right now we’re using JPA to an Oracle db. We CANNOT import
    ojdbc as dependency because of some copyright issues -.-… Temp solution
    is to add it to your local repo, then maven will fetch it there, but
    that is not in the pom just yet.
    
    Otherwise, all dependencies are ready so far, including all JPA
    dependencies.
    
    NOTE: The META-INF containing PU has been added. Right now its on my db
    for setup.
    
    Commit by: Thomas & Christopher

---
+++ src/main/java/layer/Temp.java
@@ -0,0 +1,19 @@
+package layer;
+
+import javax.persistence.*;
+
+
+public class Temp {
+
+    public static void main(String[] args) {
+        
+        System.out.println("----------------------> Project is running!");
+//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("sipPU"); 
+//        EntityManager em = emf.createEntityManager();
+        
+        
+        
+        
+    }
+    
+}
