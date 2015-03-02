ab67702cbd7ea22c634225d8f5a70ac910741e0c
ThomasHedegaard <thomashj93@gmail.com>
2015-03-02 21:54:07 +0100

    Ojdbc now works
    Correct pom dependency on ojdbc now works.
    IMPORTANT: You have to add ojdbc.jar 19.5 locally!
    
    Commit by: Thomas

--- src/main/java/layer/Temp.java
+++ src/main/java/layer/Temp.java
@@ -8,9 +8,9 @@ public class Temp {
     public static void main(String[] args) {
         
         System.out.println("----------------------> Project is running!");
-//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("sipPU"); 
-//        EntityManager em = emf.createEntityManager();
-        
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("sipPU"); 
+        EntityManager em = emf.createEntityManager();
+        System.out.println("----------------------> Project is running indeed!");
         
         
         
