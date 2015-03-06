706497e036383108f0c57ac55a5d58c4bb3a4f86
robertelving <vivalaklask@gmail.com>
2015-03-06 09:30:18 +0100

    SubjectHandler now connected with SubjectFacade
    Author: Robert

--- src/main/java/handlers/SubjectHandler.java
+++ src/main/java/handlers/SubjectHandler.java
@@ -3,22 +3,27 @@ package handlers;
 import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import facades.SubjectFacade;
 import java.io.IOException;
+import java.io.OutputStream;
+import webInterface.SubjectFacadeIF;
 
 public class SubjectHandler implements HttpHandler {
-    
+
+    SubjectFacadeIF sF;
     Gson transformer;
     //SubjectFacadeInterface subjectFacade;
     ServerResponse sr;
-    
+
     private String response;
     private int statusCode;
 
-    public SubjectHandler(Gson transformer, ServerResponse sr){
+    public SubjectHandler(Gson transformer, ServerResponse sr) {
         this.transformer = transformer;
         this.sr = sr;
+        this.sF = new SubjectFacade();
     }
-    
+
     @Override
     public void handle(HttpExchange he) throws IOException {
         String method = he.getRequestMethod().toUpperCase();
@@ -26,24 +31,32 @@ public class SubjectHandler implements HttpHandler {
         response = "";
         statusCode = 0;
         System.out.println("pathName: " + pathName);
-        
-        switch(pathName){
+
+        switch (pathName) {
             case "first":
                 handleSubject(he, method);
                 break;
-            default :
+            default:
                 sendNotFound();
-        
+
         }
-        
+
         he.getResponseHeaders().add("Content-Type", "application/json");
         sr.sendMessage(he, statusCode, response);
     }
 
-    private void handleSubject(HttpExchange he, String method) {
+    private void handleSubject(HttpExchange he, String method) throws IOException {
         System.out.println("handleSubject");
         switch (method) {
+            
             case "GET":
+                
+                String electiveSubject = this.sF.getFirstElectiveSubjects();
+
+                he.sendResponseHeaders(200, electiveSubject.length());
+                try (OutputStream os = he.getResponseBody()) {
+                    os.write(response.getBytes());
+                }
                 
                 break;
 
@@ -59,7 +72,7 @@ public class SubjectHandler implements HttpHandler {
     }
 
     private void handleSubmit(HttpExchange he, String method) {
-        
+
         System.out.println("handleSubmit");
     }
 
